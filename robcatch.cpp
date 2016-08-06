3ddc40c29fc7b198cce7e6d6f768656989976afb
rahul-soshte <rahul.soshte47@gmail.com>
2016-08-07 01:06:16 +0530

    played with threshold example

--- robcatch.cpp
+++ robcatch.cpp
@@ -7,15 +7,25 @@ int main(int argc,char** argv)
 {
 CvCapture* webcam=cvCreateCameraCapture(0);
 if(!webcam)
+{
+	//Exit with an error
+	
+}
 while(1)
 {
 Mat image=cvQueryFrame(webcam);
-}
-if(!image)
+
+if(image)
 {
 //Process it;
+//Threshold binary inverted
+
+//if there is a thief in the image use the following code
+//cvSaveImage("somefile.jpg",image,0);
+
 }
-cvSaveImage("somefile.jpg",image,0);
+}
+
 cvNamedWindow("thief",1);
 cvShowImage("Thief");
 
