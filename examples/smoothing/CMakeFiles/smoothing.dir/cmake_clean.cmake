57d8091352de0de3c55f05f595e5b4ef78d46813
rahul-soshte <rahul.soshte47@gmail.com>
2016-08-21 22:28:18 +0530

    Smoothing.cpp added

---
+++ examples/smoothing/CMakeFiles/smoothing.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/smoothing.dir/smoothing.cpp.o"
+  "smoothing.pdb"
+  "smoothing"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/smoothing.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
